bd2394905603e667cfb26eb4c529b5eb316fee1d
BrockAllen-au <brock.allen61@gmail.com>
2022-12-07 12:59:46 +1000

    Complete day 05 part 02

--- day_05/main.py
+++ day_05/main.py
@@ -13,15 +13,25 @@ def main():
     for tower in towers.values():
         tower.reverse()  # Reverse list to replicate stack to have final index as the last item in list
     print(towers)
-    process_instructions(towers, MOVE_FILE)
-    print()
+    process_instructions_01(towers, MOVE_FILE)
+    print(f"Part 01 Crates:\n")
+    display_top_crates(towers)
+    towers = load_towers(TOWER_FILE)
+    for tower in towers.values():
+        tower.reverse()  # Reverse list to replicate stack to have final index as the last item in list
+    process_instructions_02(towers, MOVE_FILE)
+    print(f"Part 02 Crates:\n")
+    display_top_crates(towers)
+
+
+def display_top_crates(towers):
     print("Crates on top:")
     for key, value in towers.items():
         print(key, value[-1])
 
 
-def process_instructions(towers, file):
-    with open(file) as in_file:
+def process_instructions_01(towers, instructions):
+    with open(instructions) as in_file:
         for line in in_file:
             instruction = line.strip().split()
             move_amount = int(instruction[AMOUNT])
@@ -30,6 +40,22 @@ def process_instructions(towers, file):
             for i in range(move_amount):
                 crate = towers[move_from].pop()
                 towers[move_to].append(crate)
+
+
+def process_instructions_02(towers, instructions):
+    with open(instructions) as in_file:
+        for line in in_file:
+            crates_to_move = []
+            instruction = line.strip().split()
+            move_amount = int(instruction[AMOUNT])
+            move_from = int(instruction[STACK_FROM])
+            move_to = int(instruction[STACK_TO])
+            for i in range(move_amount):
+                crate = towers[move_from].pop()
+                crates_to_move.append(crate)
+            crates_to_move.reverse()
+            for box in crates_to_move:
+                towers[move_to].append(box)
 
 
 def load_towers(file):
