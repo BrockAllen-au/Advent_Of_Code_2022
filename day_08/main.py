99439b7867222aa1f35c6d53befcc8f8232eb863
BrockAllen-au <brock.allen61@gmail.com>
2022-12-15 15:19:14 +1000

    Complete day_08 part_02

--- day_08/main.py
+++ day_08/main.py
@@ -6,10 +6,79 @@ def main():
     with open(FILE) as in_file:
         for line in in_file:
             content.append(line.strip())
-    outer_trees = get_outside_trees(content)
+    outer_trees = get_visible_outer_trees(content)
     inner_trees = get_visible_inner_trees(content)
     print("Part 1:")
     print("Trees Visible:", len(outer_trees) + len(inner_trees))
+    scenic_score = calculate_visible_trees(content)
+    print("Part 2:")
+    print("Top Scenic Score:", scenic_score)
+
+
+def calculate_visible_trees(content):
+    top_scenic_score = 0
+    # Check remaining inner trees
+    for i, trees in enumerate(content, 1):
+        if trees != content[0] and trees != content[-1]:
+            for j, tree in enumerate(trees):
+                if j != 0 and j != len(trees) - 1:  # Don't check outer numbers already counted in border
+                    count_trees_left = j
+                    trees_left = 0
+                    while count_trees_left != 0:
+                        if tree > trees[count_trees_left - 1] and count_trees_left == 1:
+                            trees_left += 1
+                            count_trees_left = 0
+                        elif tree > trees[count_trees_left - 1]:
+                            count_trees_left -= 1
+                            trees_left += 1
+                        elif tree <= trees[count_trees_left - 1]:
+                            count_trees_left = 0
+                            trees_left += 1
+
+                    count_trees_right = j
+                    trees_right = 0
+                    while count_trees_right != len(trees) - 1:
+                        if tree > trees[count_trees_right + 1] and count_trees_right + 1 == len(trees) - 1:
+                            count_trees_right = len(trees) - 1
+                            trees_right += 1
+                        elif tree > trees[count_trees_right + 1]:
+                            count_trees_right += 1
+                            trees_right += 1
+                        elif tree <= trees[count_trees_right + 1]:
+                            count_trees_right = len(trees) - 1
+                            trees_right += 1
+
+                    count_trees_up = i - 1
+                    trees_up = 0
+                    while count_trees_up != 0:
+                        if int(tree) > int(content[count_trees_up - 1][j]) and count_trees_up == 1:
+                            count_trees_up = 0
+                            trees_up += 1
+                        elif int(tree) > int(content[count_trees_up - 1][j]):
+                            trees_up += 1
+                            count_trees_up -= 1
+                        elif int(tree) <= int(content[count_trees_up - 1][j]):
+                            count_trees_up = 0
+                            trees_up += 1
+
+                        count_trees_down = i - 1
+                        trees_down = 0
+                        while count_trees_down != len(content) - 1:
+                            if int(tree) > int(content[count_trees_down + 1][j]) and count_trees_down + 1 == len(
+                                    content) - 1:
+                                trees_down += 1
+                                count_trees_down = len(content) - 1
+                            elif int(tree) > int(content[count_trees_down + 1][j]):
+                                count_trees_down += 1
+                                trees_down += 1
+                            elif int(tree) <= int(content[count_trees_down + 1][j]):
+                                count_trees_down = len(content) - 1
+                                trees_down += 1
+
+                        scenic_score = trees_up * trees_down * trees_left * trees_right
+                        if scenic_score > top_scenic_score:
+                            top_scenic_score = scenic_score
+    return top_scenic_score
 
 
 def get_visible_inner_trees(content):
@@ -67,7 +136,7 @@ def get_visible_inner_trees(content):
     return inner_trees
 
 
-def get_outside_trees(content):
+def get_visible_outer_trees(content):
     outside_trees = []
     # Gets all trees that are bordering the forrest and will be visible
     for i, trees in enumerate(content):
